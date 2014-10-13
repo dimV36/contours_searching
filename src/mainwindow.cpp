4021022963cf4bbe89302069ca1370ff06d9b9ea
Dmitry Voronin <carriingfate92@yandex.ru>
2014-10-13 23:43:22 +0400

    Sobel

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,12 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "QDebug"
-
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     _ui(new Ui::MainWindow) {
     _ui -> setupUi(this);
+    _threshold = 1;
 }
 
 
@@ -21,6 +22,7 @@ void MainWindow::setFileNameOnTittle(const QString &file_name) {
 
 
 void MainWindow::setInputImage(const QString &file_name) {
+    _file_name = file_name;
     _input_image.load(file_name);
     QImage image  = _input_image.scaled(_input_image.size() / 5);
     _ui -> _label_input -> setPixmap(QPixmap::fromImage(image));
@@ -55,6 +57,80 @@ bool MainWindow::save(const QString &file_name) {
 }
 
 
+QImage MainWindow::cvMatToQImage(const Mat &matrix) {
+    switch (matrix.type()) {
+       // 8-bit, 4 channel
+       case CV_8UC4: {
+          qDebug() << "rgb32";
+          QImage image(matrix.data, matrix.cols, matrix.rows, matrix.step, QImage::Format_RGB32);
+          return image;
+       }
+
+       // 8-bit, 3 channel
+       case CV_8UC3: {
+          qDebug() << "rbt8888";
+          QImage image(matrix.data, matrix.cols, matrix.rows, matrix.step, QImage::Format_RGB888 );
+          return image.rgbSwapped();
+       }
+
+       // 8-bit, 1 channel
+       case CV_8UC1: {
+          static QVector<QRgb>  color_table;
+          qDebug() << "indexed8";
+          // only create our color table once
+          if (color_table.isEmpty()) {
+             for (int i = 0; i < 256; i++)
+                color_table.push_back(qRgb(i, i, i));
+          }
+          QImage image(matrix.data, matrix.cols, matrix.rows, matrix.step, QImage::Format_Indexed8);
+          image.setColorTable(color_table);
+          return image;
+       }
+
+       default:
+          qWarning() << "ASM::cvMatToQImage() - cv::Mat image type not handled in switch:" << matrix.type();
+          break;
+    }
+    return QImage();
+}
+
+
+void MainWindow::updateOutputImage() {
+//    Mat input, copy, input_gray, detected_edges;
+    int ddepth = CV_16S;
+    Mat input = imread(windowTitle().toStdString());
+    if (NULL == input.data) {
+        QMessageBox::critical(this, tr("Ошибка загрузки изображения"),
+                              tr("Невозможно загрузить изображение из %1").arg(windowTitle()));
+
+        return;
+    }
+    GaussianBlur(input, input, Size(3, 3), 0, 0, BORDER_DEFAULT);
+    Mat input_gray;
+    cvtColor(input, input_gray, CV_BGR2GRAY);
+    Mat grad_x, grad_y;
+    Mat abs_grad_x,abs_grad_y;
+    Sobel(input_gray, grad_x, 1, 0, 3, 1, 0, BORDER_DEFAULT);
+    convertScaleAbs(grad_x, abs_grad_x);
+    Sobel(input_gray, grad_y, ddepth, 0, 1, 3, 1, 0, BORDER_DEFAULT);
+    convertScaleAbs(grad_y, abs_grad_y);
+    Mat result;
+    addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, result);
+//    copy.create(input.size(), input.type());
+//    cvtColor(input, input_gray, CV_BGR2GRAY);
+//    blur(input_gray, detected_edges, Size(3, 3));
+//    Canny(detected_edges, detected_edges, _threshold, _threshold * 3, 3);
+//    copy = Scalar::all(0);
+//    input.copyTo(copy, detected_edges);
+//    vector<int> jpeg_parametres;
+//    jpeg_parametres.push_back(CV_IMWRITE_JPEG_QUALITY);
+//    _output_image = cvMatToQImage(copy);
+    _output_image = cvMatToQImage(result);
+    QImage image = _output_image.scaled(_output_image.size() / 5);
+    _ui -> _label_output -> setPixmap(QPixmap::fromImage(image));
+}
+
+
 void MainWindow::on__action_open_triggered() {
     QString file_name = QFileDialog::getOpenFileName(this,
                                                      "Открыть изображение",
@@ -86,20 +162,11 @@ void MainWindow::on__action_save_as_triggered() {
 
 
 void MainWindow::on__action_search_countours_triggered() {
-    int low_threshold = 1;
-    cv::Mat input, copy, input_gray, detected_edges;
-    input = cv::imread(windowTitle().toStdString());
-    if (NULL == input.data) {
-        QMessageBox::critical(this, tr("Ошибка загрузки изображения"),
-                              tr("Невозможно загрузить изображение из %1").arg(windowTitle()));
+    updateOutputImage();
+}
 
-        return;
-    }
-    copy.create(input.size(), input.type());
-    cv::cvtColor(input, input_gray, CV_BGR2GRAY);
-    cv::blur(input_gray, detected_edges, cv::Size(3,3));
-    cv::Canny(detected_edges, detected_edges, low_threshold, low_threshold * 3, 3);
-    copy = cv::Scalar::all(0);
-    input.copyTo(copy, detected_edges);
-    cv::imwrite(QString('test.jpg').toStdString(), copy);
+
+void MainWindow::on__slider_threshold_valueChanged(int value) {
+    _threshold = value;
+    updateOutputImage();
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -12,6 +12,8 @@ namespace Ui {
 class MainWindow;
 }
 
+using namespace cv;
+
 class MainWindow : public QMainWindow {
     Q_OBJECT
 
@@ -27,6 +29,8 @@ private:
     void setFileNameOnTittle(const QString& file_name);
     void setInputImage(const QString& file_name);
     void updateButtons();
+    QImage cvMatToQImage(const Mat &matrix);
+    void updateOutputImage();
     bool saveAs();
     bool save(const QString& file_name);
 
@@ -34,11 +38,14 @@ private slots:
     void on__action_open_triggered();
     void on__action_save_triggered();
     void on__action_save_as_triggered();
-
     void on__action_search_countours_triggered();
+
+    void on__slider_threshold_valueChanged(int value);
 
 private:
     Ui::MainWindow *_ui;
+    QString _file_name;
+    int _threshold;
 };
 
 #endif // MAINWINDOW_H
